59b9d6c7e0c2311eee033b52501d512836db7850
Murdho <murdho@murdho.com>
2013-02-03 12:53:48 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockApp::Application.config.secret_token = '5e8b8be686b7436ab102b9c58d75f8793c4fe056d5c5b2eff639d8f6a90c1e19983689747cec6b2b1c60defc610cb1cd0e0f623c7308d5ce0bca4135eb00dd43'
